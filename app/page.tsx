d11f0f0a8f3f7458a24070fec1e0062ee3aaf3e0
Balraj Dhawan <Balraj@swivl.tech>
2026-04-10 15:39:32 +0530

    fix: removing ununsed impots

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import React, { useState } from "react";
-import Image from "next/image";
 
 export default function Home() {
   const [pageCount, setPageCount] = useState<number>(0);
